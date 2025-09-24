214c3f409e9ea98f7ce486587902a6a0cc18dc59
Shubhamt45054 <shubhamt45054@gmail.com>
2025-09-25 02:02:20 +0530

    chnages

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import app from './src/app.js';
-import { connectDB } from './src/db/connection.js';
+import { connectDB } from './src/config/connection.js';
 
 const PORT = process.env.PORT || 5000;
 
